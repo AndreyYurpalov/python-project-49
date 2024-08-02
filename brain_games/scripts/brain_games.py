42a26fc2b55caad64918d2fed42e1b122dc1e0fc
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-08-02 16:45:20 +0500

    fixed brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,9 +2,11 @@
 
 from brain_games.cli import welcome_user
 
+
 def main():
     print("Welcome to the Brain Games!")
-    name = welcome_user("May I have your name? ")
-    
+    welcome_user("May I have your name? ")
+
+
 if __name__ == '__main__':
     main()
